cfe8824b5d3d03939f4fd2c4cddd80f5c6af45a4
Michał Brusiło <mibru@interia.pl>
2020-02-25 12:04:21 +0100

    change idea and finish app

--- main.js
+++ main.js
@@ -1,12 +1,58 @@
+// const removeTask = (e) => {
+//  // e.target.remove();
+//  // e.target.parentNode.remove();
+//  // console.log(e.target.parentNode);
+//  // e.target.parentNode.style.textDecoration = "line-through";
+//  // e.target.remove();
+//  const index = e.target.dataset.key;
+//  document.querySelector(`li[data-key="${index}"]`).remove()
+
+// }
+
+// document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask))
+
+
+const toDoList = [];
+
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const taskNumber = document.querySelector('h1 span');
+const listItems = document.getElementsByClassName('task');
+const input = document.querySelector('input');
+
 const removeTask = (e) => {
- // e.target.remove();
- // e.target.parentNode.remove();
- // console.log(e.target.parentNode);
- // e.target.parentNode.style.textDecoration = "line-through";
- // e.target.remove();
- const index = e.target.dataset.key;
- document.querySelector(`li[data-key="${index}"]`).remove()
+ e.target.parentNode.remove();
+ const index = e.target.parentNode.dataset.key;
+ toDoList.splice(index, 1)
+ console.log(toDoList);
+ taskNumber.textContent = listItems.length;
+ renderList();
+}
+
+
+const addTask = (e) => {
+ e.preventDefault()
+ const titleTask = input.value;
+ if (titleTask === "") return;
+ const task = document.createElement('li');
+ task.className = 'task';
+ task.innerHTML = titleTask + "<button>Usuń</button>";
+ toDoList.push(task)
+ renderList()
+
+ ul.appendChild(task);
+ input.value = "";
+ // const liItems = document.querySelectorAll('li.task').length;
+ taskNumber.textContent = listItems.length;
+ task.querySelector('button').addEventListener('click', removeTask);
 
 }
 
-document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask))
+const renderList = () => {
+ ul.textContent = "";
+ toDoList.forEach((toDoElement, key) => {
+  toDoElement.dataset.key = key;
+  ul.appendChild(toDoElement);
+ })
+}
+form.addEventListener('submit', addTask)
